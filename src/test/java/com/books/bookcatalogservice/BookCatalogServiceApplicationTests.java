4a2c3a604b3e54969a26ec907b7e63accaa99260
sriram <sriram@FULL624-PRO.local>
2021-02-20 14:25:37 +0530

    fixed issue

--- src/test/java/com/books/bookcatalogservice/BookCatalogServiceApplicationTests.java
+++ src/test/java/com/books/bookcatalogservice/BookCatalogServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.books.bookcatalogservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookCatalogServiceApplicationTests {
 
 	@Test
